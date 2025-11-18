e5c52759e08ccc1daf38bb5db47cb9ccf28787e4
Abdurrahman Almouna <109709044+Almonaabdo@users.noreply.github.com>
2025-11-17 21:07:05 -0500

    connected to server

--- M-01/WorkStation/MainWindow.xaml.cs
+++ M-01/WorkStation/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SqlDataAdapter = Microsoft.Data.SqlClient.SqlDataAdapter;
 
 namespace WorkStation
 {
@@ -16,9 +19,77 @@ namespace WorkStation
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string? connectionString = "Data Source=localhost;Initial Catalog=advsql-milestone-2;Integrated Security=True;TrustServerCertificate=True;";
+        private SqlDataAdapter adapter = new SqlDataAdapter();
+        private DataTable dataTable = new DataTable();
+        SqlConnection connection = new SqlConnection();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        /*
+         * FUNCTION : test_connection()
+         * PURPOSE  : tests connection to servera nd database by the provided connection string in the APP config
+         */
+        private void test_connection()
+        {
+            try
+            {
+                if (easyConnect.IsChecked == false)
+                {
+                    connectionString = "Data Source=6.tcp.ngrok.io,16946;Initial Catalog=advsql-milestone-2;User ID=abdal;Password=abdal;Encrypt=False;TrustServerCertificate=True;";
+                }
+                connection = new SqlConnection(connectionString);
+                connection.Open();
+                //connection.Close();
+                connectionButton.Visibility= Visibility.Hidden;
+                onlineStatus.Visibility = Visibility.Visible;
+                easyConnect.Visibility = Visibility.Hidden;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private void read_quantity(string partName)
+        {
+            string query = "SELECT value FROM APP_CONFIG WHERE configDescription = @desc";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@desc", partName);
+
+            try
+            {
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        MessageBox.Show($"Quantity: {reader["value"]}");
+                    }
+                    else
+                    {
+                        MessageBox.Show("No matching config found.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            read_quantity("Lens");
+        }
+
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            test_connection();
         }
     }
 }
